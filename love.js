1024aaf799e2503379856bece38d1ee94f918cb4
Streaming2024 <workforyubraj@gmail.com>
2025-09-25 17:40:32 +0545

    Create love.js

---
+++ love.js
@@ -0,0 +1,36 @@
+// streams.js
+const streamMap = {
+  "Fox": {
+    url: "https://hugh.cdn.rumble.cloud/live/v0xi25uh/slot-85/xop9-bbmz/chunklist_DVR.m3u8",
+    poster: "#"
+  },
+  "willow": {
+    url: "https://rumble.com/live-hls-dvr/6x7rjo/playlist.m3u8?",
+    poster: "#"
+  },
+  "Fox2": {
+    url: "https://hugh.cdn.rumble.cloud/live/d0t27del/slot-155/mmil-w7nl/chunklist.m3u8",
+    poster: "#"
+  },
+  "illow": {
+    url: "https://amg01269-amg01269c1-willowtv-us-5479.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-willowtvus/playlist.m3u8",
+    poster: ""
+  },
+  "WTV": {
+    url: "https://rumble.com/live-hls/6x2vag/playlist.m3u8",
+    poster: ""
+  },
+  "S1": {
+    url: "https://hugh.cdn.rumble.cloud/live/v0xi25uh/slot-85/xop9-bbmz/chunklist_DVR.m3u8",
+    poster: "#"
+  },
+    "S2": {
+    url: "https://hugh.cdn.rumble.cloud/live/r8wvl35k/slot-115/3mdj-dc92/chunklist.m3u8",
+    poster: ""
+  },
+  "S5": {
+    url: "https://dai.google.com/linear/hls/event/inq9igrMQ32gRZqcAKG_3w/master.m3u8",
+    poster: ""
+  },
+
+};
