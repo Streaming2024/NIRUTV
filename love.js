7793d296028bedc21feb9c3f0d37f026841f93e7
Streaming2024 <workforyubraj@gmail.com>
2025-09-25 17:42:33 +0545

    Update love.js

--- love.js
+++ love.js
@@ -5,7 +5,7 @@ const streamMap = {
     poster: "#"
   },
   "willow": {
-    url: "https://rumble.com/live-hls-dvr/6x7rjo/playlist.m3u8?",
+    url: "https://rumble.com/live-hls-dvr/6x95p4/playlist.m3u8?",
     poster: "#"
   },
   "Fox2": {
