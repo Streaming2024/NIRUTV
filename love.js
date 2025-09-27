efd217c54971e27eac4731ddfee792ff7af682c8
Streaming2024 <workforyubraj@gmail.com>
2025-09-27 20:21:08 +0545

    Update love.js

--- love.js
+++ love.js
@@ -12,8 +12,8 @@ const streamMap = {
     url: "https://hugh.cdn.rumble.cloud/live/d0t27del/slot-155/mmil-w7nl/chunklist.m3u8",
     poster: "#"
   },
-  "illow": {
-    url: "https://amg01269-amg01269c1-willowtv-us-5479.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-willowtvus/playlist.m3u8",
+  "NP3": {
+    url: "https://ktvhdsg.ekantipur.com:8443/high_quality_85840165/hd/playlist.m3u8 ",
     poster: ""
   },
   "WTV": {
