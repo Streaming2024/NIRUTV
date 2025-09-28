287d2544cfeac5966618848239aff80fa2a4f7d6
Streaming2024 <workforyubraj@gmail.com>
2025-09-28 15:58:31 +0545

    Update love.js

--- love.js
+++ love.js
@@ -2,11 +2,11 @@
 const streamMap = {
   "NP": {
     url: "https://hugh.cdn.rumble.cloud/live/r31jzt5k/slot-141/d8g9-by2i/chunklist.m3u8",
-    poster: "#"
+    poster: "https://en.himalpress.com/wp-content/uploads/2025/09/Unity-Cup.jpg"
   },
   "NP2": {
     url: "https://hugh.cdn.rumble.cloud/live/k5e12sb4/slot-83/t7p4-n07p/chunklist.m3u8",
-    poster: "#"
+    poster: "https://www.fancode.com/skillup-uploads/cms-media/West-Indies-vs-Nepal-in-UAE-old-match-card.png"
   },
   "Fox2": {
     url: "https://hugh.cdn.rumble.cloud/live/d0t27del/slot-155/mmil-w7nl/chunklist.m3u8",
