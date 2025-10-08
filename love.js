3adb51a209021092fd796bd9044aa4d4c1b8986d
Streaming2024 <workforyubraj@gmail.com>
2025-10-08 12:19:09 +0545

    Update love.js

--- love.js
+++ love.js
@@ -8,20 +8,15 @@ const streamMap = {
     url: "https://ktvhdsg.ekantipur.com:8443/ktvmax2025/match1/playlist.m3u8",
     poster: "https://yonotv.pages.dev/NEPvsKW.png"
   },
-  "NP3": {
-    url: "https://hugh.cdn.rumble.cloud/live/v13wgnv6/slot-49/4m4k-4qjy/chunklist.m3u8",
+  "ICC2": {
+    url: "https://rumble-foxo.cdn.rumble.cloud/live/k5e12sb4/slot-91/daan-3di0/chunklist_DVR.m3u8",
     poster: "https://yonotv.pages.dev/NEPvsKW.png"
-
-  },
-
-  "WC": {
-    url: "https://fl7.moveonjoy.com/WILLOW_CRICKET/tracks-v1a1/mono.ts.m3u8",
-    poster: "https://imgeng.jagran.com/images/2025/09/30/article/image/complete-list-of-ICC-women-cricket-world-cup-winners--1759237177116.webp"
   },
   
   "ICC": {
     url: "https://hugh.cdn.rumble.cloud/live/k5e12sb4/slot-91/daan-3di0/chunklist.m3u8",
     poster: "https://nirutv.online/logo/image.png"
+  
   },
   "Myco": {
     url: "https://cdn2.ml-pull-dvc-myco.io:2096/ICC-WWC-MATCH-3/index.m3u8",
